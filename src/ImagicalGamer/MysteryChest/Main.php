c5cb337afe0574c8a5338c668043b3b51366bbfc
Joe [ERPE] <JoeycraftPE@gmail.com>
2016-06-28 09:08:15 -0500

    Update and rename src/ImagicalGamer/MysteryChests/Main.php to src/ImagicalGamer/MysteryChest/Main.php

--- src/ImagicalGamer/MysteryChest/Main.php
+++ src/ImagicalGamer/MysteryChest/Main.php
@@ -8,7 +8,6 @@ use pocketmine\plugin\Plugin;
 use pocketmine\Server;
 use pocketmine\Player;
 
-use pocketmine\event\player\PlayerDeathEvent;
 use pocketmine\event\player\PlayerInteractEvent;
 
 use pocketmine\utils\Config;
@@ -21,7 +20,7 @@ use pocketmine\utils\TextFormat as C;
 
 use pocketmine\math\Vector3;
 use pocketmine\level\Level;
-use pocketmine\level\particle\DustParticle;
+use pocketmine\level\particle\FlameParticle;
 
 /* Copyright (C) ImagicalGamer - All Rights Reserved
  * Unauthorized copying of this file, via any medium is strictly prohibited
@@ -36,19 +35,8 @@ class Main extends PluginBase implements Listener{
     $config = new Config($this->getDataFolder() . "/config.yml", Config::YAML);
     $config->save();
     $this->getServer()->getPluginManager()->registerEvents($this, $this);
-    //$this->economy = $this->getServer()->getPluginManager()->getPlugin("EconomyAPI");
-    //$this->api = EconomyAPI::getInstance ();
   }
 
-  public function onDeath(PlayerDeathEvent $event){
-    $entity = $event->getEntity();
-    $cause = $entity->getLastDamageCause();
-    if($entity instanceof Player){
-       if($cause instanceof Player){
-        $killer->getInventory()->addItem(Item::get(388,0,1));
-    }
-  }
-}
   public function onInteract(PlayerInteractEvent $event){
     $block = $event->getBlock();
     $player = $event->getPlayer();
@@ -56,13 +44,8 @@ class Main extends PluginBase implements Listener{
       if($block->getId() === Block::CHEST){     
         if($inventory->contains(new Emerald(0,1))) {
         $event->setCancelled();
-        $player->sendMessage(C::GREEN . "Opening a MysteryChest...");
-        sleep(1);
-        $player->sendMessage(C::RED . C::BOLD . "3");
-        sleep(1);
-        $player->sendMessage(C::RED . C::BOLD . "2");
-        sleep(1);
-        $player->sendMessage(C::RED . C::BOLD . "1");
+        $player->sendMessage(C::AQUA . "Opening a MysteryChest...I wonder whats inside!");
+
         $level = $player->getLevel();
         $x = $block->getX();
         $y = $block->getY();
@@ -73,7 +56,7 @@ class Main extends PluginBase implements Listener{
         $center = new Vector3($x+1, $y, $z);
         $radius = 0.5;
         $count = 100;
-        $particle = new DustParticle($center, $r, $g, $b, 1);
+        $particle = new FlameParticle($center, $r, $g, $b, 1);
           for($yaw = 0, $y = $center->y; $y < $center->y + 4; $yaw += (M_PI * 2) / 20, $y += 1 / 20){
               $x = -sin($yaw) + $center->x;
               $z = cos($yaw) + $center->z;
@@ -83,22 +66,34 @@ class Main extends PluginBase implements Listener{
         $prize = rand(1,6);
         switch($prize){
         case 1:
-          $inventory->addItem(Item::get(293,0,1));
+          $inventory->addItem(Item::get(22,0,1));
+          $this->getServer()->broadcastMessage("§e---[•§2Crates§e•]---");
+          $this->getServer()->broadcastMessage("§7•The player§b " . $player->getName() . "§7found §a1 Lapis Block§7!•");
         break;
         case 2:
-          $inventory->addItem(Item::get(293,0,1));
+          $inventory->addItem(Item::get(364,0,8));
+          $this->getServer()->broadcastMessage("§e---[•§2Crates§e•]---");
+          $this->getServer()->broadcastMessage("§7•The player§b " . $player->getName() . "§7found §a8 Steak§7!•");
         break;   
         case 3:
-          $inventory->addItem(Item::get(293,0,1));
+          $inventory->addItem(Item::get(266,0,9));
+          $this->getServer()->broadcastMessage("§e---[§•2Crates§e•]---");
+          $this->getServer()->broadcastMessage("§7•The player§b " . $player->getName() . "§7found §a9 Gold Ingots§7!•");
         break;   
         case 4:
-          $inventory->addItem(Item::get(293,0,1));
+          $inventory->addItem(Item::get(265,0,7));
+          $this->getServer()->broadcastMessage("§e---[•§2Crates§e•]---");
+          $this->getServer()->broadcastMessage("§7•The player§b " . $player->getName() . "§7found §a7 Iron Ingots§7!•");
         break;      
         case 5:
-          $inventory->addItem(Item::get(293,0,1));
+          $inventory->addItem(Item::get(264,0,5));
+          $this->getServer()->broadcastMessage("§e--[•§2Crates§e•]---");
+          $this->getServer()->broadcastMessage("•§7The player§b " . $player->getName() . "§7found §a5 Diamonds§7!•");
         break;     
         case 6:
-          $inventory->addItem(Item::get(293,0,1));  
+          $inventory->addItem(Item::get(339,1000,1));
+          $this->getServer()->broadcastMessage("§e---[•§2Crates§e•]---");
+          $this->getServer()->broadcastMessage("•§7The player§b " . $player->getName() . "§7 found§a $1000§7 in MoneyNote form!•");
         break;
     }
   }
